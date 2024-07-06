59cfa1c7fcb85087f0b84866b18766cc9a29ce46
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-07-06 19:22:20 +0530

    Add themes - Add common UI components - Add openAIgraph, twiiter, Logo images - Add gitignoer - Add site config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,64 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { siteConfig } from '@/config/site';
+import Image from 'next/image';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'coinXtracker',
-  description: 'Regular crypto insights and data',
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: [
+    {
+      name: 'Akshay Shinde',
+      url: 'https://akshayshinde.com',
+    },
+  ],
+  creator: 'Akshay Shinde',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@folklore69_',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  // manifest: `${siteConfig.url}/site.webmanifest`,
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 };
 
 export default function RootLayout({
@@ -17,7 +69,29 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {process.env.NEXT_DEV_MODE !== 'PROD' ? (
+            children
+          ) : (
+            <main className="flex h-screen justify-center items-center">
+              <Image
+                className="h-auto w-auto"
+                src="/images/coinXtracker.jpg"
+                alt="Next.js Logo"
+                width={180}
+                height={37}
+                priority
+              />
+            </main>
+          )}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ config/site.ts
@@ -0,0 +1,24 @@
+export const siteConfig = {
+  name: 'coinXtracker',
+  url: 'https://coinxtracker.com',
+  ogImage: 'https://coinxtracker.com/images/coinXtracker.png',
+  description: 'Regular crypto insights and data',
+  links: {
+    twitter: 'https://x.com/folklore69_',
+    github: 'https://github.com/alphacoder-mp3/coinxtracker',
+  },
+  keywords: [
+    'Crypto',
+    'Coins',
+    'Coins prediction',
+    'Coin',
+    'Next.js',
+    'React',
+    'Tailwind CSS',
+    'Server Components',
+    'Radix UI',
+    'block chain',
+  ],
+};
+
+export type SiteConfig = typeof siteConfig;
